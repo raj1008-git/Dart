c27397975d9ceb5abc55664065f3f4fb265fb588
raj1008-git <macof.rj@gmail.com>
2025-09-20 19:09:43 +0545

    Closure functions and Memory part of the closure functions.

--- functions/closure_functions.dart
+++ functions/closure_functions.dart
@@ -1,19 +1,57 @@
 // Closure Functions //
 
 // This is a Regular Function.
-void sayHello() {
-  print('Hello');
-}
+// void sayHello() {
+//   print('Hello');
+// }
 
-void executeFunction(void Function() function) {
-  print('About to run a function..');
-  function();
+// void executeFunction(void Function() function) {
+//   print('About to run a function..');
+//   function();
+// }
+
+// void main() {
+//   // Function assigned to a variable
+//   void Function() greetingFunction = sayHello;
+
+//   greetingFunction();
+//   executeFunction(greetingFunction);
+// }
+
+// Traditional Approach. -> You Tell Each waiter exactly what to do.
+// void serveMorningShift() {
+//   print('Good Morning! Coffee today?');
+// }
+
+// void serveEveningShift() {
+//   print('Good Evening! Wine with dinner?');
+// }
+
+// //Closure Approach. -> You give each waiter the greeting card they use.
+
+// void Function() morningGreeting = () => print('Good Morning! Coffee today?');
+// void Function() eveningGreeting = () =>
+//     print('Good Evening! Wine with Dinner?');
+
+// void waiterServes(void Function() greeting) {
+//   greeting();
+// }
+
+// void main() {
+//   waiterServes(morningGreeting);
+//   waiterServes(eveningGreeting);
+// }
+
+Function createGreeter(String name) {
+  return () {
+    print('Hello, $name!');
+  };
 }
 
 void main() {
-  // Function assigned to a variable
-  void Function() greetingFunction = sayHello;
+  Function greetJohn = createGreeter('John');
+  Function greetSarah = createGreeter('Sarah');
 
-  greetingFunction();
-  executeFunction(greetingFunction);
+  greetJohn();
+  greetSarah();
 }
