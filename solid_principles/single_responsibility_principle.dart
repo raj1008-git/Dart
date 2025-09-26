b2467d95df62deda232bbf5a9522db2a466a5e69
raj1008-git <macof.rj@gmail.com>
2025-09-26 10:46:48 +0545

    single responsibiity without using abstraction

--- solid_principles/single_responsibility_principle.dart
+++ solid_principles/single_responsibility_principle.dart
@@ -1,33 +1,69 @@
+// without abstraction problem will emerge,
 class User {
-  String name;
-  String email;
-  String password;
+  final String name;
+  final String email;
+  final String password;
 
   User({required this.name, required this.email, required this.password});
+}
 
-  bool validateEmail() {
+class UserValidator {
+  bool validateEmail(String email) {
     return email.contains('@') && email.contains('.');
   }
 
-  bool validatePassword() {
+  bool validatePassword(String password) {
     return password.length >= 8;
   }
+}
 
-  Future<void> saveToDatabase() async {
-    print('Connecting to Database');
-    print('Saving user: $name, $email');
+// Concrete class no abstraction.
+class MySQLUserRepository {
+  Future<void> save(User user) async {
+    print('Saving to mySql database');
   }
 
-  Future<User?> loadFromDatabase(String email) async {
-    print('Loading user with email');
+  Future<User?> findByEmail(String email) async {
+    print('Querring mysql for $email');
     return null;
   }
+}
 
-  Future<void> sendWelcomeEmail() async {
-    print('sending Welcome email, to $email');
-  }
-
-  String encryptPassword() {
-    return 'encrypted $password';
+class SMTPEmailService {
+  Future<void> sendWelcomeEmail(User user) async {
+    print('Sending via SMTP to : ${user.email}');
   }
 }
+
+// The UserService is now Tightly Coupled to specific Implementations.
+class UserService {
+  final UserValidator validator;
+  final MySQLUserRepository repository;
+  final SMTPEmailService emailService;
+
+  UserService({
+    required this.validator,
+    required this.repository,
+    required this.emailService,
+  });
+
+  Future<bool> registerUser(User user) async {
+    if (!validator.validateEmail(user.email)) return false;
+
+    await repository.save(user);
+    await emailService.sendWelcomeEmail(user);
+    return true;
+  }
+}
+
+void main() {
+  final userService = UserService(
+    validator: UserValidator(),
+    repository: MySQLUserRepository(),
+    emailService: SMTPEmailService(),
+  );
+}
+
+// Here you are locked into these specific implementations.
+//Must be MySql
+// MUST be SMTP
