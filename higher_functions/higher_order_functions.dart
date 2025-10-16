13b8435de3038f13553bd0f675718da910d48653
raj1008-git <macof.rj@gmail.com>
2025-10-16 13:36:44 +0545

    Extra Filters

--- higher_functions/higher_order_functions.dart
+++ higher_functions/higher_order_functions.dart
@@ -1,21 +1,11 @@
-// Multiple Filters.
+// Other Filter Functions.
 void main() {
   var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  // Filter Even Numbers.
-  var evens = numbers.where((n) => n % 2 == 0).toList();
-  print('Evens: $evens');
+  // firstWhere -get First match.
+  var firstWhere = numbers.firstWhere((n) => n % 2 == 0);
 
-  var greaterThanFive = numbers.where((n) => n > 5).toList();
-  print('Greater than 5: $greaterThanFive');
+  var firstOver20 = numbers.firstWhere((n) => n > 20, orElse: () => -1);
 
-  // Combine filters
-  var evenAndGreaterThanFive = numbers
-      .where((n) => n % 2 == 0)
-      .where((n) => n > 5)
-      .toList();
-
-  var evenOrGreaterThanEight = numbers
-      .where((n) => n % 2 == 0 || n > 8)
-      .toList();
+  var lastEven = numbers.lastWhere((n) => n % 2 == 0);
 }
